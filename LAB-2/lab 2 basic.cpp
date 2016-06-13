5e4d78311e98edf01345ca3ae1f755f80548986f
ABBATnull <mrabbat69@gmail.com>
2016-06-13 14:19:27 +0700

    Create lab 2 basic.cpp

---
+++ LAB-2/lab 2 basic.cpp
@@ -0,0 +1,22 @@
+#include <clocale>
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+	 
+using namespace std;
+ 
+
+	 
+int main()
+	{     
+	
+double sum, razn, proizv, sred;
+int a, b;	 
+
+int i = 5; i = ++i+ ++i;
+cout<<i;
+
+
+	               system("pause");       
+	                 return 0;
+	}
